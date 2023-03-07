03330c02f5090cfb6c8153234d1c183dc20511fd
unknown <troysteedmiller@gmail.com>
2023-03-06 22:15:23 -0800

    copied from laptop to pc and is now functional

--- assets/sql/schema.sql
+++ assets/sql/schema.sql
@@ -1,27 +1,24 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
-
-USE employees;
-
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE DATABASE employee_trackerDB;
+USE employee_trackerDB;
 CREATE TABLE department (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department VARCHAR(30)
+ 
 );
-
-CREATE TABLE role(
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(50) UNIQUE NOT NULL,
-    salary DECIMAL UNSIGNED NOT NULL,
-    department_id INT UNSIGNED NOT NULL,
-    INDEX dep_ind (department_id),
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
-
-CREATE TABLE employee(
-    id INTEGER AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INTEGER(10),
-    manager_id INTEGER(10) NULL,
-    PRIMARY KEY (id)
-)
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
